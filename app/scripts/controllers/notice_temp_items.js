c79c8457c7ad0e64e24e42e8c678cef249aebb9b
seongyu <suengyu@me.com>
2014-08-08 15:39:34 +0900

    금융리스 업데이트

--- app/scripts/controllers/notice_temp_items.js
+++ app/scripts/controllers/notice_temp_items.js
@@ -92,98 +92,20 @@ var notice_items = [{
 
 
 
-function Notice_Controller($scope) {
-    $scope.items = notice_items;
-
-
-
+function Notice_Controller($scope, noticeService) {
     $scope.itemsPerPage = 5;
     $scope.pagedItems = [];
     $scope.currentPage = 0;
-
     var getlist = function () {
-        var searchMatch = function (haystack, needle) {
-            if (!needle) {
-                return true;
-            }
-            return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
-        };
-        // init the filtered items
-        $scope.search = function () {
-            $scope.currentPage = 0;
-            // now group by pages
-            $scope.groupToPages();
-        };
-        // calculate page in place
-        $scope.groupToPages = function () {
-            $scope.pagedItems = [];
-
-            for (var i = 0; i < $scope.items.length; i++) {
-                if (i % $scope.itemsPerPage === 0) {
-                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.items[i] ];
-                } else {
-                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.items[i]);
-                }
-            }
-        };
-        $scope.range = function (start, end) {
-            var ret = [];
-            if (!end) {
-                end = start;
-                start = 0;
-            }
-            for (var i = start; i < end; i++) {
-                ret.push(i);
-            }
-            return ret;
-        };
-        $scope.prevPage = function () {
-            if ($scope.currentPage > 0) {
-                $scope.currentPage--;
-            }
-        };
-        $scope.nextPage = function () {
-            if ($scope.currentPage < $scope.pagedItems.length - 1) {
-                $scope.currentPage++;
-            }
-        };
-        $scope.setPage = function () {
-            $scope.currentPage = this.n;
-        };
-        // functions have been describe process the data for display
-        $scope.search();
-    };
-    getlist();
-}
-function Notice_Detail($scope, $routeParams) {
-    $scope.notice_items = notice_items[$routeParams.id];
-    $scope.notice_items_sorting = $scope.notice_items.images.split('/');
-}
-
-/*
-'use strict';
-function listview($scope, $filter, items){
-    $scope.noticetitle="제목테스트";
-    $scope.pageClass = 'ctrlAni';
-    $scope.itemsPerPage = 5;
-    $scope.pagedItems = [];
-    $scope.currentPage = 0;
-
-    $scope.isAdmin = function () {
-        return SessionInfo.getCurrentUser().isAdmin;
-    };
-
-     $scope.process = function (mole) {
-        $scope.firstitem = mole;
-        console.log(mole);
-    };
-
-    var getlist = function () {
-        items.query().then(function (result) {
-            $scope.firstitem = result[0];
+        noticeService.list().then(function (result) {
             $scope.items = result;
         }).then(function () {
-
+            var searchMatch = function (haystack, needle) {
+                if (!needle) {
+                    return true;
+                }
+                return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+            };
             // init the filtered items
             $scope.search = function () {
                 $scope.currentPage = 0;
@@ -228,35 +150,46 @@ function listview($scope, $filter, items){
             };
             // functions have been describe process the data for display
             $scope.search();
-        })
-    };
-    getlist();
-
-    $scope.update = function (item) {
-
-        var i = item.id;
-        items.getone(i).then(function (result) {
-            $scope.item = result[0];
         });
-
-        $scope.insert = function () {
-            var title = $scope.item.title;
-            var content = $scope.item.content;
-
-            items.update(i, title, content).then(function (data) {
-                alert(data);
-                history.back();
-            })
-        }
-    };
-
-    $scope.delete = function (id, fileN) {
-        items.delete(id, fileN).then(function (data) {
-            alert(data);
-            getlist();
-        })
     }
+    getlist();
+}
+function Notice_Detail($scope, $routeParams, noticeService) {
+    var i = $routeParams.id;
+    noticeService.one(i).then(function(result){
+        $scope.notice_items = result[0];
+    });
+    //$scope.notice_items_sorting = $scope.notice_items.images.split('/');
 
 }
 
-*/
+function Notice_Write($scope, noticeService){
+    var $filess;
+    $scope.onFileSelect = function($files) {
+        $filess = $files;
+    };
+    $scope.insert = function(){
+        if($filess==null || $filess==''){
+            var title = $scope.item.title;
+            var content = $scope.item.content;
+            var images = 'no file';
+            noticeService.insert(title, content, images).then(function(data){
+                alert(data)
+            });
+            history.back();
+        }else{
+            noticeService.noticeInsertF($filess).then(function(result){
+                var title = $scope.item.title;
+                var writer = $scope.item.writer;
+                var content = $scope.item.content;
+                var images = result;
+                items.insert(title, writer, content, images).then(function(data){
+                    alert(data)
+                });
+                history.back();
+            },function(){
+                alert('잘못된 등록이 발생하였습니다.')
+            });
+        }
+    };
+}
